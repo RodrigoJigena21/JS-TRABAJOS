1a08abd0c074034c9836c9659b2fa468f2b02c4b
Rodrigo Jigena <rodrigo.jigena2108@gmail.com>
2022-09-29 08:28:08 -0300

    Simulador interactivo

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-        
-//Variables
-        const precioDeHamburguesa = 200
-        const descuento = 0.89
-        let unidades, personas
-        let nombre = (prompt("Bienvenido a hambuerguerseria ELJIGE! Ingrese su nombre.")).toLowerCase();
-//Nombre de la persona y cantidad de hambuerguesas
-        do {
-            unidades = parseFloat(prompt(`Hola ${nombre}!!!! Cuantas hamburguesas quiere llevar?`))
-        if (isNaN(unidades)) {
-            alert("Por favor ingresa un número válido")
-        }
-        } while (isNaN(unidades))
-     
-        do { 
-            personas = parseFloat(prompt("Cuantas personas van a comer?"))
-        if(isNaN(personas)) {
-            alert("Por favor ingresa un número válido")
-        }
-        } while (isNaN(personas))
-//Descuento por cupon
-        cupon = parseInt(prompt("Tiene cupon de descuento?"))
+const elegirMoneda =prompt('ingrese moneda a convertir a pesos arg (1) DOLAR - (2) EURO')
+const monto =parseInt(prompt('Ingrese monto a calcualar'))
 
-        if (cupon == "si") {
-            total = (( costoDeHamburguesa - (costoDeHamburguesa * descuento) ) * personas) * unidades
-            console.log(total)
-        } else {
-            total = (costoDeHamburguesa * personas) * unidades
-            reserva = parseInt(prompt(`Debe de pagar un tota de $ ${total} pesos. Quiere continuar comprando?`)).toLowerCase()
-        } 
-        if (compra == "no") {
-            prompt("Vulva cuando quiera!")
-        }
+
+let dolar = 286
+let euro = 149
+
+
+switch(elegirMoneda){
+    case '1':
+       let resultado =  monto * dolar
+        alert('valor de conversion es de ' + resultado + ' de pesos arg ' )
+        break;
+
+        case '2':
+           let resultado2 = monto * euro
+            alert('valor de conversion es de ' + resultado2 + ' de pesos arg ' )
+            break
+}
